9d1195c46849b3a3420a684f6da426f746b81229
Jijesh Mohan <jijeshmohan@gmail.com>
2012-11-23 16:49:17 +0530

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuditWorkflow::Application.config.secret_token = '56b1b14f733c8b75ab572f949597b273368e29e13ac653f650e69d5d372d460fe3c375a7d86af98a0e2bb5199974a948ebca6f0010b33b42baf949fba207bf48'
